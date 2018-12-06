cc22fc7a2fe6c255e9be74cfd53396f3db9dfde3
shamanth-hs <shamanthhs494@gmail.com>
2018-12-06 19:31:43 +0530

    Update Candidate.py

--- Candidate.py
+++ Candidate.py
@@ -54,11 +54,11 @@ def candidate_elimination(examples):
     print("\n S[{0}]:".format(i), S)
     for xcx in examples:
         i = i + 1
-        x, cx = xcx[:-1], xcx[-1]  # Splitting data into attributes and decisions
-        if cx == 'Y':  # x is positive example
+        x, cx = xcx[:-1], xcx[-1]  
+        if cx == 'Y': 
             G = {g for g in G if fulfills(x, g)}
             S = generalize_S(x, G, S)
-        else:  # x is negative example
+        else:  
             S = {s for s in S if not fulfills(x, s)}
             G = specialize_G(x, domains, G, S)
         print("\n G[{0}]:".format(i), G)
@@ -72,10 +72,10 @@ def generalize_S(x, G, S):
         if not fulfills(x, s):
             S.remove(s)
             Splus = min_generalizations(s, x)
-            ## keep only generalizations that have a counterpart in G
+            
             S.update([h for h in Splus if any([more_general(g,h)
                                                for g in G])])
-            ## remove hypotheses less specific than any other in S
+            
             S.difference_update([h for h in S if
                                  any([more_general(h, h1)
                                       for h1 in S if h != h1])])
@@ -88,10 +88,10 @@ def specialize_G(x, domains, G, S):
         if fulfills(x, g):
             G.remove(g)
             Gminus = min_specializations(g, domains, x)
-            ## keep only specializations that have a conuterpart in S
+            
             G.update([h for h in Gminus if any([more_general(h, s)
                                                 for s in S])])
-            ## remove hypotheses less general than any other in G
+            
             G.difference_update([h for h in G if
                                  any([more_general(g1, h)
                                       for g1 in G if h != g1])])
